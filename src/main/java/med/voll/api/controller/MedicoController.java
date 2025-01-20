b874987262b03ee5d21c1ce7467410e8eeb9723e
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2025-01-20 09:56:29 -0300

    Adicionadas funcionalidades de paginação na listagem de médicos e pacientes, configurada a anotação @pageableDefault para definir novos padrões de paginação e ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,9 @@ import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoDTO;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +28,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listarMedicos() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listarMedicos( @PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
+        return repository.findAll(pageable).map(DadosListagemMedico::new);
     }
 
 
--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
-@Getter
+//@Getter
 @EqualsAndHashCode(of = "id")
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "Paciente")
-@Table(name = "pacientes")
+@Table(name = "paciente")
 public class Paciente {
 
     @Id
@@ -34,5 +34,32 @@ public class Paciente {
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
     }
+//-------------------------------------------------------------------------------------------------------------
+    public Paciente(){
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
 }
