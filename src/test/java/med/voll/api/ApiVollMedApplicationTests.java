612fe86ba6d529c5d222890ac32bbfcb4dfb5c56
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2025-02-02 00:16:40 -0300

    "Spring security adicionado"

--- src/test/java/med/voll/api/ApiVollMedApplicationTests.java
+++ src/test/java/med/voll/api/ApiVollMedApplicationTests.java
@@ -1,4 +1,4 @@
-package med.voll.api;
+/*package med.voll.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class ApiVollMedApplicationTests {
     void contextLoads() {
     }
 
-}
+}*/
