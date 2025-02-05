ae8be5cab7746b30e4d67f5527a27e6a0e3b0766
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2025-02-04 23:14:24 -0300

    apromoramento classes de segunraca e inicio da tokenizacao

---
+++ src/main/resources/db/migration/V6__insert-into-usuario.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (login, senha) VALUES (
+                                                   'jooao.pedro@voll.med',
+                                                   '$2a$12$waZefrWtTV104wVsmOwOKeE0994nC4HNc7BF5m0ZoYr2oENq36h2a'
+                                               );
