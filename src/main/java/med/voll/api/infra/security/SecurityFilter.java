1328d2c4536fb85239b422ee5e649b3c5cb8d0c9
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2025-02-05 22:54:48 -0300

    Correcao de classes

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -1,21 +1,38 @@
 package med.voll.api.infra.security;
 
-import jakarta.servlet.Filter;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
-public class SecurityFilter extends OncePerRequestFilter {
+    @Component
+    public class SecurityFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Security Filter");
-        filterChain.doFilter(request, response);
+
+        @Autowired
+        private TokenService tokenService;
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            System.out.println("Security Filter");
+            var tokenJWT = recuperarToken(request);
+            var subject = tokenService.getSubject(tokenJWT);
+
+            filterChain.doFilter(request, response);
+        }
+
+        private String recuperarToken(HttpServletRequest request) {
+
+            var token = request.getHeader("Authorization");
+            if(token ==null){
+                throw new RuntimeException("Token invalido");
+            }
+            return token.replace("Bearer ", "");
+        }
     }
-}
